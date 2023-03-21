af3df3846da660a591c1b8f4b38d317fc87afb5d
Yves Plansson <yclpjr@gmail.com>
2023-03-21 09:11:58 -0300

    Função Divisão

--- funcaoSoma.js
+++ funcaoSoma.js
@@ -5,18 +5,12 @@ function soma(a, b) {
 function sub(a, b) {
   return a - b;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1.0.1
 
 function mult(a, b) {
   return a * b;
 }
-<<<<<<< HEAD
->>>>>>> 1.0.0
-=======
-=======
->>>>>>> 859102705e8c7e1e2a94367571307e8782f5a06e
->>>>>>> 1.0.1
+
+function division(a, b) {
+  if (!b) return 0;
+  return a / b;
+}
